620d88c74b56633fbb67aa80e3b72824d412f397
lain alien <104214732+lainAlien@users.noreply.github.com>
2024-02-04 20:41:43 -0500

    big redo
    rebuilt the whole thing using baaasically the same file picking thing, added a file picker, some more flags, and based it around the warmplace jsplay page rather than the basic example

--- player.php
+++ player.php
@@ -1,32 +1,114 @@
 <!DOCTYPE html>
 <html>
     <?php //sunvox file picker
+        $svDir = glob('projects/*.sunvox');
+        //including e as a flag in the url hides (or, doesnt build) the file picker
+        if (is_null($_GET['e'])) {
+            // build file chooser to be blitted out later
+            $chooser = "<table class='chooser'>";
+            $rowNum = 0;
+            foreach($svDir as $x) {
+                if ( $rowNum % 2 != 0) { $darker = " class='darker' "; } else { $darker = ""; }
+                $chooser .= "<tr" . $darker . ">";
+                //list of files, directory and extension stripped, dashes replaced with spaces
+                    $chooser .= "<td class='play'><a href='#' onclick=\"fname = '" . $x . "'; load(fname); return false;\">⯈&emsp;" . str_replace("-", " ", substr($x,9,-7)) . "</a></td>";
+                //permalinks
+                    $chooser .= "<td class='links'><a href='../sunvox?p=" . substr($x,9) . "'>☍</a></td>"; //unicode U+260D
+                //downloads if d isn't set
+                    if (is_null($_GET['d'])) {
+                        $chooser .= "<td class='links'><a href='projects/" . substr($x,9) . "'>⯯</a></td>"; //unicode U+2BEF
+                    }
+                $chooser .= "</tr>";
+                $rowNum = $rowNum + 1;
+            }
+            $chooser .= "</table>";
+        }
         //including the name of ur file in the url after ?p= should play a specific file
         if (isset($_GET['p'])) {
             $svFile = 'projects/' . $_GET['p'];
         } else {
-            $svDir = glob('projects/*.sunvox');
     	    $svFile = $svDir[array_rand($svDir)];
         }
     ?>
     <head>
-        <link href="player.css" rel="stylesheet"/>
-        <script src="js/sunvox.js"></script>
-        <script src="js/sunvox_lib_loader.js"></script>
-        <script>
-        </script>
+        <link href="../css/player.css" rel="stylesheet"/>
+        <script src="../js/sunvox/sunvox.js"></script>
+        <script src="../js/sunvox/sunvox_lib_loader.js"></script>
     </head>
     <body>
-        <div class="boxen">
-            <div class="playBox"><button type="button" onclick="svToggle();" class="playButton" id="svController"><big>sound or no</big></button></div>
-            <div class="sliderBox"><input type="range" min="1" max="255" value="80" class="slider" id="svVolSlider" oninput="sv_volume(0, this.value);"></div>
-        </div>
+        <form action="">
+            <button type="button" onclick="sv_play_from_beginning(0); playStatus = 1; info();" class="playButton" id="svController">play</button>
+            <button type="button" onclick="sv_stop(0); playStatus = 0;" class="playButton" id="svController">unplay</button>
+            <input type="range" min="1" max="255" value="80" class="slider" id="svVolSlider" oninput="sv_volume(0, this.value);">
+        </form>
+        <p id="status">initializing...</p>
+        <p id="info"></p>
+        <br />
+        <?php echo $chooser; ?>
+
+        <!-- this, taken apart
+        and reassembled, hollow
+          with no more vision
+
+        https://warmplace.ru/soft/sunvox/jsplay/
+
+        !-->
         <script>
-            var svProject = "<?php echo $svFile; ?>";
-            var svSlider = document.getElementById("svVolSlider");
-            var svButton = document.getElementById("svController");
-            var svVolDefault = 80;
-            function status( s ) { console.log( s ); }
+            var fileSize = 0;
+            var numLines = 1;
+            var playStatus = 0;
+            var fname = "<?php echo $svFile; ?>"
+            function status( s ) { document.getElementById( "status" ).innerHTML = s; console.log( s ); }
+            function info() { //Show song information:
+                // bodged to only show verbose info with the ?v tag (song name and filename only otherwise)
+                <?php if (isset($_GET['v'])) {echo 'var s = "File info:<br>";';} ?>
+                <?php if (isset($_GET['v'])) {echo 's += "size: " + fileSize + " bytes;<br>";';} ?>
+                s += "name: " + sv_get_song_name( 0 ) + ";<br>";
+                s += "filename: " + fname.substr(9) + ";<br>";
+                <?php if (isset($_GET['v'])) {echo 's += "BPM (Beats Per Minute): " + sv_get_song_bpm( 0 ) + ";<br>";';} ?>
+                <?php if (isset($_GET['v'])) {echo 's += "TPL (Ticks Per Line or Tempo): " + sv_get_song_tpl( 0 ) + ";<br>";';} ?>
+                <?php if (isset($_GET['v'])) {echo 's += "number of frames: " + sv_get_song_length_frames( 0 ) + ";<br>";';} ?>
+                <?php if (isset($_GET['v'])) {echo 'numLines = sv_get_song_length_lines( 0 );';} ?>
+                <?php if (isset($_GET['v'])) {echo 's += "number of lines: " + numLines + ";<br>";';} ?>
+                <?php if (isset($_GET['v'])) {echo 'var mm = sv_get_number_of_modules( 0 );';} ?>
+                <?php if (isset($_GET['v'])) {echo 's += "number of modules: " + mm + ";<br>";';} ?>
+                <?php if (isset($_GET['v'])) {echo 'var pp = sv_get_number_of_patterns( 0 );';} ?>
+                <?php if (isset($_GET['v'])) {echo 's += "number of patterns: " + pp + ";<br>";';} ?>
+                <?php if (isset($_GET['v'])) {echo 's += "<pre>Log:\n" + sv_get_log( 1024 ) + "</pre>";';} ?>
+                document.getElementById( "info" ).innerHTML = s;
+            }
+            function loadFromArrayBuffer( buf ) {
+                if( buf ) {
+                    var byteArray = new Uint8Array( buf );
+                    if( sv_load_from_memory( 0, byteArray ) == 0 ) {
+                        centerReq = 1;
+                        fileSize = byteArray.byteLength;
+                        status( "song loaded" );
+                        info();
+                        if( playStatus ) {
+                            sv_play_from_beginning( 0 );
+                        }
+                    } else {
+                        status( "song load error" );
+                    }
+                }
+            }
+            function load( fname ) {
+                status( "loading: " + fname );
+                var req = new XMLHttpRequest();
+                req.open( "GET", fname, true );
+                req.responseType = "arraybuffer";
+                req.onload = function( e ) {
+                    if( this.status != 200 ) {
+                        status( "file not found" );
+                        return;
+                    }
+                    var arrayBuffer = this.response;
+                    loadFromArrayBuffer( arrayBuffer );
+                };
+                req.send( null );
+            }
+            //Start SunVox:
             svlib.then( function(Module) {
                 //
                 // SunVox Library was successfully loaded.
@@ -35,59 +117,23 @@
                 svlib = Module;
                 status( "SunVoxLib loading is complete" );
                 var ver = sv_init( 0, 44100, 2, 0 ); //Global sound system init
-                if( ver >= 0 )
-                {
-	            status( "init ok" );
-                }
-                else
-                {
-	            status( "init error" );
-	            return;
+                if( ver >= 0 ) {
+                    //Show information about the library:
+                    var major = ( ver >> 16 ) & 255;
+                    var minor1 = ( ver >> 8 ) & 255;
+                    var minor2 = ( ver ) & 255;
+                    console.log( "SunVox lib version: " + major + " " + minor1 + " " + minor2 );
+                    status( "init ok" );
+                } else {
+                    status( "init error" );
+                    return;
                 }
                 sv_open_slot( 0 ); //Open sound slot 0 for SunVox; you can use several slots simultaneously (each slot with its own SunVox engine)
                 //
                 // Try to load and play some SunVox file:
                 //
-                status( "loading project..." );
-                var req = new XMLHttpRequest();
-                req.open( "GET", svProject, true );
-                req.responseType = "arraybuffer";
-                req.onload = function( e ) 
-                {
-	            if( this.status != 200 )
-	            {
-	                status( "error code: " + this.status );
-	                return; //good luck if this happens lol
-	            }
-	            var arrayBuffer = this.response;
-	            if( arrayBuffer ) 
-	            {
-	                var byteArray = new Uint8Array( arrayBuffer );
-	                if( sv_load_from_memory( 0, byteArray ) == 0 )
-	                {
-	    	        fileSize = byteArray.byteLength;
-	    	        status( "song loaded" );
-                    svVolDefault = sv_volume(0);
-                    svSlider.value = svVolDefault;
-                    sv_volume(0, svVolDefault);
-	                }
-	                else
-	                {
-	    	        status( "song load error" );
-	                }
-	            }
-                };
-                req.send( null );
+                load( fname );
             } );
-            function svToggle() {
-                if(sv_end_of_song(0)===1) {
-                    sv_play_from_beginning(0);
-                    svButton.class="stopButton";
-                } else {
-                    sv_stop(0); sv_stop(0);
-                    svButton.class="playButton";
-                }
-            }
         </script>
     </body>
 </html>
